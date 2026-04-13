34f5483176eb3b72830503f797153db44ce552f6
Sara Spadafina <spadafina.sara@gmail.com>
2026-04-13 13:45:34 +0200

    sale e pepe dei nuovi e vecchi users

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
     // protected $guarded = [];
 
     protected $fillable = [
-        'name','email','password', 'avatar'
+        'name','email','password', 'avatar', "salt"
     ];
 
     /**
